0301e6415f3304fe23067c52b0b0d15aa91c65f7
Alice Tiertant <alice.tiertant@gmail.com>
2025-04-22 15:08:03 +0200

    Ajout Exercices 1

---
+++ Exercices 1.php
@@ -0,0 +1,35 @@
+<?php
+
+//Exercice 1
+$name = 'Alice';
+echo $name, PHP_EOL;
+
+//Exercice 2
+$firstName = 'Alice';
+$lastName = 'Tiertant';
+echo "Bonjour, je m'appelle $firstName $lastName", PHP_EOL;
+
+//Exercice 3
+$a = 5;
+$b = 3;
+echo $a + $b, PHP_EOL;
+echo $a - $b, PHP_EOL;
+echo $a * $b, PHP_EOL;
+echo $a / $b, PHP_EOL;
+
+//Exercice 4
+$isLoggedIn = false;
+echo "Utilisateur connecté : $isLoggedIn", PHP_EOL;
+$isLoggedIn = true;
+echo "Utilisateur connecté : $isLoggedIn", PHP_EOL;
+
+//Exercice 5
+$variable = "test";
+$mavariable = "exemple";
+$class = "PHP";
+
+//Exercice 6
+$temperature = 32;
+var_dump($temperature);
+$temperature = "Trente-deux";
+var_dump($temperature);
